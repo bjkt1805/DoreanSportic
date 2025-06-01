f8c06f4860752590eb4945b12f21e1686d0afb4a
bjkt1805 <bjkt1805@gmail.com>
2025-06-01 13:40:17 -0600

    Añadir AutoMapper y actualizar las referencias del proyecto
    Se actualizaron los archivos de proyecto de `DoreanSportic.Application`, `DoreanSportic.Infrastructure` y `DoreanSportic.Web` para incluir la versión `AutoMapper` 14.0.0` y establecer dependencias entre proyectos. Se añadieron las referencias de archivos generados en `DoreanSportic.Infrastructure.csproj`.
    
    Se modificó `appsettings.Development.json` para incluir una nueva cadena de conexión de SQL Server.
    
    Se actualizó `site.css` para comentar la línea del complemento `daisyui`. Se introdujo la clase `DoreanSporticContext` para la gestión del contexto de la base de datos de Entity Framework.

---
+++ DoreanSportic.Infrastructure/Data/DoreanSporticContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoreanSportic.Infrastructure.Data;
+
+public partial class DoreanSporticContext : DbContext
+{
+    public DoreanSporticContext(DbContextOptions<DoreanSporticContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
