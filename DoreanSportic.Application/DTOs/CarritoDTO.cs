b2630b34c3ffa51cef423d16c4b6e4ffd8e0b760
bjkt1805 <bjkt1805@gmail.com>
2025-06-07 17:57:01 -0600

    Refactorización de la estructura del proyecto y actualización de los componentes de la interfaz de usuario.
    - Se actualizó `DoreanSportic.Application.csproj` para incluir nuevas referencias a las carpetas `Services\Implementations` y `Services\Interfaces`, eliminando `DTOs` y `Services`.
    - Se mejoró `_Layout.cshtml` para mejorar la estructura HTML, la accesibilidad y la navegación, incluyendo actualizaciones del logotipo y la entrada de búsqueda.
    - Se cambió el tema predeterminado en `site.css` de oscuro a claro, manteniendo la preferencia de tema oscuro.
    - Se realizaron varias actualizaciones en `site.min.css` para los estilos de menú horizontal, la alineación de la barra de navegación y la funcionalidad de intercambio mejorada.
    - Se añadió la nueva clase DTO `CarritoDTO` en `CarritoDTO.cs` con propiedades para `IdAutor`, `Nombre` y una lista de `Libro`.
    - Se incluyó un comando en `comandoContext.txt` para el andamiaje de un contexto de base de datos con opciones de conexión específicas.

---
+++ DoreanSportic.Application/DTOs/CarritoDTO.cs
@@ -0,0 +1,15 @@
+﻿using DoreanSportic.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Libreria.Application.DTOs
+{
+    public record CarritoDTO
+    {
+        public int IdAutor { get; set; }
+        public string Nombre { get; set; } = null!;
+        public virtual List<Carrito> Libro { get; set; } = new List<Carrito>();
+    }
+}
