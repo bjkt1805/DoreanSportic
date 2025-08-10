0ccf59375c61130a678f0037db71013ac0f32778
bjkt1805 <bjkt1805@gmail.com>
2025-08-09 20:20:24 -0600

    Se agregaron las validaciones iniciales de modelo en el formulario de login

--- DoreanSportic/ViewModels/LoginViewModel.cs
+++ DoreanSportic/ViewModels/LoginViewModel.cs
@@ -5,13 +5,10 @@ namespace DoreanSportic.Web.ViewModels
     {
         [Display(Name = "Usuario")]
         [Required(ErrorMessage = "{0} es requerido")]
-        [DataType(DataType.EmailAddress)]
         public string UserName { get; set; } = default!;
 
-        [StringLength(15, MinimumLength = 6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres y no sobrepasar los 15 caracteres")]
-        [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Solo se permiten números y letras")]
-        [Required(ErrorMessage = "{0} es requerida")]
         [Display(Name = "Contraseña")]
+        [Required(ErrorMessage = "{0} es requerida")]
         public string Password { get; set; } = default!;
     }
 }
